a3b78588621062bc99040eaf1090da2f728e5aa2
Vivek-Prajapati1 <vivekanurag30@gmail.com>
2025-06-13 02:53:15 +0530

    first init

---
+++ 01basicreact/src/Chai.js
@@ -0,0 +1,10 @@
+import { h2 } from "framer-motion/m";
+
+function Chai() {
+  return (
+    <h2>chai in react </h2>
+  )
+}
+
+export default Chai;
+
---
+++ 01vitereact/src/main.jsx
@@ -0,0 +1,52 @@
+import { StrictMode } from 'react'
+import React from 'react';
+import { createRoot } from 'react-dom/client'
+
+import App from './App.jsx'
+
+
+function MyApp(){
+    return (
+        <div>
+            <h1>Custom !</h1>
+        </div>
+    )
+}
+
+
+
+// const reactElement ={
+//     type: 'a',
+//     props: {
+//         href: 'https://www.google.com',
+//         target: '_blank',
+// },
+// children: 'Click me to visit Google'
+
+// }
+
+
+
+const anotherElement = (
+    <a href="https://google.com" target='_blank'>Visit google</a>
+)
+
+const anotherUser = "chai aur code"
+
+const reactElement = React.createElement (
+    'a',
+    {href: 'https://google.com',target: '_blank'},
+    'click me to visit google',
+     
+
+)
+
+createRoot(document.getElementById('root')).
+render(
+
+    
+    
+   reactElement
+
+
+);
